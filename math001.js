0a03096629eec5807bdb71f6e19c688b4f1608a6
mavasquez401 <mavasquez401@gmail.com>
2024-05-08 19:54:24 -0400

    Completed the multiplier

---
+++ math001.js
@@ -0,0 +1,13 @@
+multiplyThem = () => {
+    event.preventDefault();
+    // Pulls numbers from the HTML first
+    let number1 = document.getElementById("num1").value
+    let number2 = document.getElementById("num2").value
+
+    // Multiplies the numbers and converts string to number
+    let multipliedNum = parseFloat(number1 * number2)
+
+    // Changes totalMultiplies to new text
+    document.getElementById("totalMultiplied").innerHTML = `<p style="color:Green">The total of ${number1} times ${number2} equals ${multipliedNum}</p>`
+    console.log(multipliedNum)
+}
